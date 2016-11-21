1cc51d954e10c620692140c04fd048eee127a628
acff <acff@cin.ufpe.br>
2016-11-21 11:05:31 -0300

    Added artist spider

---
+++ lyrics_music/lyrics_music/spiders/artistspider.py
@@ -0,0 +1,21 @@
+from scrapy.spiders import Spider
+from lyrics_music.items import ArtistMusicItem
+from scrapy.http    import Request
+import re
+
+#scrapy crawl artistspider  --set CLOSESPIDER_PAGECOUNT=5 
+
+class LyricsSpider(Spider):
+	name = "artistspider"
+	allowed_domains = ["letras.mus.br"]
+	start_urls = ["https://www.letras.mus.br/mais-acessadas/mpb/"]
+
+	def parse(self, response):
+		links = response.xpath("//ol[@class='top-list_art']/li/a/@href").extract() #artist page
+		items = []
+		for link in links:
+			link = "https://www.letras.mus.br" + link
+			item = ArtistMusicItem()
+			item["link"] = link	
+			items.append(item)
+		return items
