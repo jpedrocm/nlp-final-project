a30cc86a96230acecba7ee323b601156880acd9f
jpedrocm <jpcm@cin.ufpe.br>
2017-01-04 16:51:52 -0300

    Final analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,7 @@
 import json
 
 def get_data():
-	with open('metrics_file.json') as data_file:    
+	with open('metrics_file_tunning_new.json') as data_file:    
 		data = json.load(data_file)
 		return data
 
@@ -27,10 +27,23 @@ def analyze():
 	data_filt = filter_strategies(data, stem = None, rmv_stopwords = None, folding = None, feature = None, clf = "LOGISTIC REGRESSION DEFAULT")
 	data_sort = sort_strategies(data_filt, decreasing=True)
 	fi = open('analysis.txt', 'w')
-	for item in data_sort:
-		fi.write(str(item['test_details']))
-		fi.write(str(item['classifier_metrics'][2]['general']['accuracy']))
+	best_item = data_sort[0]
+	fi.write(str(best_item['test_details']))
+	fi.write('\n\n')
+	for item in best_item['genres_metrics']:
+		fi.write(str(item))
 		fi.write('\n')
+	
+	#mean_acc = 0
+	#for item in data_sort:
+	#	acc = item['classifier_metrics'][2]['general']['accuracy']
+	#	fi.write(str(item['test_details']))
+	#	fi.write(str(acc))
+	#	fi.write('\n')
+	#	mean_acc+=acc
+	#mean_acc = mean_acc/32.0
+	#fi.write('\n')
+	#fi.write('MEAN_ACC: '+ str(mean_acc))
 	fi.close()
 
 analyze()
